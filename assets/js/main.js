b3fd6f7e09cb0ae47e4e15398470471a2e8c7364
jirajin <jirajin43@gmail.com>
2023-08-03 16:39:38 +0600

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -14132,7 +14132,7 @@
         return r.ready = function() {
             var r, n, o, s = a.attr("data-wf-status"),
                 p = a.attr("data-wf-domain") || "";
-            /\.webflow\.io$/i.test(p) && l.hostname !== p && (s = !0), s && !h && (e = e || (r = t('#').attr("href", "https://webflow.com?utm_campaign=brandjs"), n = t("<img>").attr("src", "#").attr("alt", "").css({
+            /\.webflow\.io$/i.test(p) && l.hostname !== p && (s = !0), s && !h && (e = e || (r = t('<a class="w-webflow-badge"></a>').attr("href", "https://webflow.com?utm_campaign=brandjs"), n = t("<img>").attr("src", "#").attr("alt", "").css({
                 marginRight: "8px",
                 width: "16px"
             }), o = t("<img>").attr("src", "#").attr("alt", "."), r.append(n, o), r[0]), f(), setTimeout(f, 500), t(i).off(c, u).on(c, u))
